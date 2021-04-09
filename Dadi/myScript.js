a37d0c6733554c966f23f18c6f955b268aef5e8b
Cordovani <lor.cordovani@gmail.com>
2021-04-09 16:49:39 +0200

    output sull' HTML

--- Dadi/myScript.js
+++ Dadi/myScript.js
@@ -11,9 +11,27 @@ console.log(tiroUtente);
 var tiroComputer = Math.round(Math.random() * (6 - 1) + 1);
 console.log(tiroComputer)
 
-//compariamo gli eventuali casi e determiniamo il vincitore
+/*
+compariamo gli eventuali casi e determiniamo il vincitore
+possiamo avere un pareggio
+facciamo sapere chi ha vinto nella pagina e console
+*/
 
-//facciamo sapere chi ha vinto 
+// vince il computer
+if (tiroComputer > tiroUtente) {
+    console.log("il vincitore è il Computer con lo strardinario punteggi di: " + tiroComputer);
+    document.getElementById("myH1").innerHTML = "il vincitore è il Computer con lo strardinario punteggi di: " + tiroComputer;
+
+//vince l'utente 
+} else if (tiroUtente > tiroComputer) {
+    console.log("il vincitore è l'Utente con lo strardinario punteggi di: " + tiroUtente);
+    document.getElementById("myH1").innerHTML = "il vincitore è l'Utente con lo strardinario punteggi di: " + tiroUtente;
+
+} else {
+// pareggio
+console.log("pareggio tra uomo e macchina");
+document.getElementById("myH1").innerHTML = "il " + tiroUtente +" sancisce il pareggio tra uomo e macchina";
+}
 
 
 
